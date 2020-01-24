f635fd7bd3ed2c38da253850a9c070a486684aa8
Shelley Vohr <shelley.vohr@gmail.com>
2020-01-23 21:12:34 -0800

    use textColor for automatic adaptability

--- shell/browser/browser_mac.mm
+++ shell/browser/browser_mac.mm
@@ -379,18 +379,10 @@ void Browser::ShowAboutPanel() {
   // Credits must be a NSAttributedString instead of NSString
   NSString* credits = (NSString*)options[@"Credits"];
   if (credits != nil) {
-    // Check if app is running in dark mode
-    NSString* mode = [[NSUserDefaults standardUserDefaults]
-        stringForKey:@"AppleInterfaceStyle"];
-    BOOL isDarkMode = [mode isEqualToString:@"Dark"];
-
-    // Set color of credits depending on if we're in dark mode or not.
-    NSColor* color = isDarkMode ? [NSColor whiteColor] : [NSColor blackColor];
     NSAttributedString* creditString = [[NSAttributedString alloc]
         initWithString:credits
-            attributes:@{NSForegroundColorAttributeName : color}];
+            attributes:@{NSForegroundColorAttributeName : [NSColor textColor]}];
 
-    // Cast back to NSDictionary with updated options
     NSMutableDictionary* mutable_options = [options mutableCopy];
     mutable_options[@"Credits"] = creditString;
     options = [NSDictionary dictionaryWithDictionary:mutable_options];
